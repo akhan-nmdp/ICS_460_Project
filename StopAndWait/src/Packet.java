cc3f6d0219f2b5b07052ad9e3e6cc2d3f63e8a82
tendhar44 <tendhar44@gmail.com>
2017-02-21 19:20:39 -0600

    Packet class

---
+++ StopAndWait/src/Packet.java
@@ -0,0 +1,9 @@
+
+public class Packet {
+
+	short cksum;		//16bit 2-byte
+	short len;			//16-bit 2-byte
+	int ackno;			//32-bit 4-byte
+	int seqno;			//32-bit 4-byte Data packet Only
+	byte data[];		//0-500 bytes. Data packet only. Variable
+}
